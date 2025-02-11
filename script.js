14909491ca37798b64b5aaecf1bdb74fbb18ca0e
Nigami-light <mihailkuntasov38@gmail.com>
2025-02-11 10:34:53 +0300

    Added main files

---
+++ script.js
@@ -0,0 +1,16 @@
+let myPidor = [
+    "Т",
+    "Ы",
+    "П",
+    "И",
+    "Д",
+    "О",
+    "Р",
+];
+
+let urlAnimate = () => {
+    window.location.hash = myPidor[Math.floor((Date.now() / 100) % myPidor.length)];
+    setTimeout(urlAnimate, 1000);
+};
+
+urlAnimate();
